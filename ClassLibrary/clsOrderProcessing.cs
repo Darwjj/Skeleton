8fc6b6ed8a818d0879c46239dc53fee51f022fe5
Darwjj <santiagoandrangopulupa7@gmail.com>
2022-02-11 15:01:04 +0000

    Add project files.

---
+++ ClassLibrary/clsOrderProcessing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrderProcessing
+    {
+        public string Description { get; set; }
+
+        public string Valid(string nDescription)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OrderProcessingTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrderProcessingTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrderProcessingTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f822218-c8df-489f-833b-e4d17a3498e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
