1c747cb24d522931221c8c72b876ee90f903c94f
Akshat Cala <akshat.jitu9@gmail.com>
2022-02-22 10:29:50 +0000

    No Errors on Staff Testing but has Errors on Order Processing.

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public int StaffID { get; set; }
+        public bool Staff_Gender { get; set; }
+        public string Staff_Role { get; set; }
+        public DateTime Staff_StartDate { get; set; }
+        public double Staff_Salary { get; set; }
+        public string Staff_FullName { get; set; }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblStaff] (
+
+    [StaffID]        INT          IDENTITY (1, 1) NOT NULL,
+
+    [Staff_FullName] VARCHAR (30) NULL,
+
+    [Staff_Gender]   VARCHAR (1)  NULL,
+
+    [Staff_Role]     VARCHAR (25) NULL,
+
+    [Staff_JoinDate] DATE         NULL,
+
+    [Staff_Salary]   MONEY        NULL,
+
+    PRIMARY KEY CLUSTERED ([StaffID] ASC)
+
+);
+
+ 
