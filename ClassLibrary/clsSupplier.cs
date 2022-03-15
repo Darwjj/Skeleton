34f31f44285c2cda3b3d7a7d2fdb30ca59c3f758
Buzuamlak Medhanie <P2560339@my365.dmu.ac.uk>
2022-03-15 13:09:52 +0000

    make some changes

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -5,15 +5,11 @@ namespace ClassLibrary
     public class clsSupplier
     {
 
-        //public bool TestData;
-       // public DateTime DateRegistered;
+        
         public string ASupplierName;
         public string ASupplierAddress;
-        //public int ASupplierID;
-        //public int ASupplierId;
         public int ASupplierID;
         public string Email;
-        //public bool Availability;
         public bool ASupplierAvailability;
         public DateTime DateOfRegister;
     }
